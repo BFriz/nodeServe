e7a461a3a39ab21d0728a8b9b67a7249de455ff7
Ben Frisby <bfrisb@gmail.com>
2016-07-28 14:52:41 -0700

    init

---
+++ server.js
@@ -0,0 +1,40 @@
+// Node.js has core modules to create http/https servers, hence we have to import the http module in order to create an HTTP Server.
+var http = require('http');
+// File I/O is provided by simple wrappers around standard POSIX functions. To use this module do require('fs'). All the methods have asynchronous and synchronous forms.
+var fs = require('fs');
+// The url module provides utilities for URL resolution and parsing.
+var url = require('url');
+
+
+// Create a server
+http.createServer( function (request, response) {
+//Where does the console.log show up?  
+  console.log(url);
+   // Parse the request containing file name
+   var pathname = url.parse(request.url).pathname;
+   // Print the name of the file for which request is made.
+   console.log("Request for " + pathname + " received.");
+   
+   // Read the requested file content from file system
+   fs.readFile(pathname.substr(1), function (err, data) {
+      if (err) {
+         console.log(err);
+         // HTTP Status: 404 : NOT FOUND
+         // Content Type: text/plain
+         response.writeHead(404, {'Content-Type': 'text/html'});
+      }else{  
+         //Page found   
+         // HTTP Status: 200 : OK
+         // Content Type: text/plain
+         response.writeHead(200, {'Content-Type': 'text/html'});  
+         
+         // Write the content of the file to response body
+         response.write(data.toString());   
+      }
+      // Send the response body 
+      response.end();
+   });   
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
